fa76a5c1d8af387732c1cad3a3df8fea59c3d406
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-10 19:01:51 +0200

    Add UseCase interface for product management with methods for creating, updating, retrieving, and searching products

---
+++ product-microservice/internal/product/usecase.go
@@ -0,0 +1,15 @@
+package product
+
+import (
+	"context"
+
+	"github.com/chuuch/product-microservice/internal/models"
+)
+
+// UseCase product
+type UseCase interface {
+	CreateProduct(ctx context.Context, product *models.Product) (*models.Product, error)
+	UpdateProduct(ctx context.Context, product *models.Product) (*models.Product, error)
+	GetProductByID(ctx context.Context, productID string) (*models.Product, error)
+	SearchProducts(ctx context.Context, query string, page, size int64) ([]*models.Product, error)
+}
