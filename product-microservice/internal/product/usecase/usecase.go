bc4e5a9678c419494ab81584e66a833c32e711f8
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-11 21:43:58 +0200

    Add use case implementation for product management, including method stubs for creating, updating, retrieving, and searching products.

---
+++ product-microservice/internal/product/usecase/usecase.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/chuuch/product-microservice/internal/models"
+	"github.com/chuuch/product-microservice/internal/product"
+	"github.com/chuuch/product-microservice/pkg/logger"
+)
+
+type productUC struct {
+	productRepo product.MongoRepository
+	log         logger.Logger
+}
+
+func NewProductUC(productRepo product.MongoRepository, log logger.Logger) *productUC {
+	return &productUC{
+		productRepo: productRepo,
+		log:         log,
+	}
+}
+
+func (u *productUC) CreateProduct(ctx context.Context, product *models.Product) (*models.Product, error) {
+	panic("not implemented")
+}
+
+func (u *productUC) UpdateProduct(ctx context.Context, product *models.Product) (*models.Product, error) {
+	panic("not implemented")
+}
+
+func (u *productUC) GetProductByID(ctx context.Context, productID string) (*models.Product, error) {
+	panic("not implemented")
+}
+
+func (u *productUC) SearchProducts(ctx context.Context, query string, page, size int64) ([]*models.Product, error) {
+	panic("not implemented")
+}
