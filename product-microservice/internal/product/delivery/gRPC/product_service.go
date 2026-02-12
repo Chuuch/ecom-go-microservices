a7e938098cd8fe16f458a2a946b22b4fea4f2882
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-12 21:30:51 +0200

    Implement CreateProduct method in ProductGRPCService, adding validation, error handling, and OpenTracing support for product creation requests.

--- product-microservice/internal/product/delivery/gRPC/product_service.go
+++ product-microservice/internal/product/delivery/gRPC/product_service.go
@@ -3,10 +3,14 @@ package grpc
 import (
 	"context"
 
+	"github.com/chuuch/product-microservice/internal/models"
 	"github.com/chuuch/product-microservice/internal/product"
+	grpcerrors "github.com/chuuch/product-microservice/pkg/grpc_errors"
 	"github.com/chuuch/product-microservice/pkg/logger"
 	productService "github.com/chuuch/product-microservice/proto/product"
 	"github.com/go-playground/validator/v10"
+	"github.com/opentracing/opentracing-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // ProductGRPCService gRPC service
@@ -27,7 +31,39 @@ func NewProductGRPCService(productUC product.UseCase, log logger.Logger, validat
 }
 
 func (p *ProductGRPCService) CreateProduct(ctx context.Context, req *productService.CreateRequest) (*productService.CreateResponse, error) {
-	panic("not implemented")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ProductGRPCService.CreateProduct")
+	defer span.Finish()
+
+	catID, err := primitive.ObjectIDFromHex(req.GetCategoryId())
+	if err != nil {
+		p.log.Errorf("primitive.ObjectIDFromHex: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	product := &models.Product{
+		CategoryID:  catID,
+		Name:        req.GetName(),
+		Description: req.GetDescription(),
+		Price:       req.GetPrice(),
+		ImageURL:    req.GetImageUrl(),
+		Photos:      req.GetPhotos(),
+		Quantity:    req.GetQuantity(),
+		Rating:      int64(req.GetRating()),
+	}
+
+	if err := p.validate.StructCtx(ctx, product); err != nil {
+		p.log.Errorf("validate.StructCtx: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	created, err := p.productUC.CreateProduct(ctx, product)
+	if err != nil {
+		p.log.Errorf("productUC.CreateProduct: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+	return &productService.CreateResponse{
+		Product: created.ToProto(),
+	}, nil
 }
 
 func (p *ProductGRPCService) UpdateProduct(ctx context.Context, req *productService.UpdateRequest) (*productService.UpdateResponse, error) {
