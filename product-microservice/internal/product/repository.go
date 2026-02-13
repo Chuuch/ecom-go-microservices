4d03a3694cf66c3aa3c57e2e2b1ab3b1cd3bcdc2
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-13 20:54:47 +0200

    Update GetProductByID method in MongoRepository to accept primitive.ObjectID instead of string, enhancing type safety for product retrieval.

--- product-microservice/internal/product/repository.go
+++ product-microservice/internal/product/repository.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/chuuch/product-microservice/internal/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Product repository interface
 type MongoRepository interface {
 	CreateProduct(ctx context.Context, product *models.Product) (*models.Product, error)
 	UpdateProduct(ctx context.Context, product *models.Product) (*models.Product, error)
-	GetProductByID(ctx context.Context, productID string) (*models.Product, error)
+	GetProductByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error)
 	SearchProducts(ctx context.Context, query string, page, size int64) ([]*models.Product, error)
 }
