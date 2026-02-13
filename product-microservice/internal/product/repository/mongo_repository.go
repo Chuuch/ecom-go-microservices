0e3e186d77b861c38437eea5cd623563b1aae77b
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-13 21:19:14 +0200

    Enhance product repository with OpenTracing support and implement CreateProduct, UpdateProduct, and GetProductByID methods. Update timestamps to UTC for consistency and improve error handling in database operations.

--- product-microservice/internal/product/repository/mongo_repository.go
+++ product-microservice/internal/product/repository/mongo_repository.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/chuuch/product-microservice/internal/models"
 	productErrors "github.com/chuuch/product-microservice/pkg/product_errors"
+	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -33,10 +35,13 @@ func NewProductMongoRepository(mongoDB *mongo.Client) *productMongoRepo {
 
 // NewProductMongoRepository
 func (p *productMongoRepo) CreateProduct(ctx context.Context, product *models.Product) (*models.Product, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "productMongoRepo.CreateProduct")
+	defer span.Finish()
+
 	collection := p.mongoDB.Database(productsDB).Collection(productsCollection)
 
-	product.CreatedAt = time.Now()
-	product.UpdatedAt = time.Now()
+	product.CreatedAt = time.Now().UTC()
+	product.UpdatedAt = time.Now().UTC()
 
 	result, err := collection.InsertOne(ctx, product, &options.InsertOneOptions{})
 	if err != nil {
@@ -55,11 +60,44 @@ func (p *productMongoRepo) CreateProduct(ctx context.Context, product *models.Pr
 }
 
 func (p *productMongoRepo) UpdateProduct(ctx context.Context, product *models.Product) (*models.Product, error) {
-	panic("not implemented")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "productMongoRepo.UpdateProduct")
+	defer span.Finish()
+
+	collection := p.mongoDB.Database(productsDB).Collection(productsCollection)
+
+	upsert := true
+	after := options.After
+	opts := options.FindOneAndUpdateOptions{
+		ReturnDocument: &after,
+		Upsert:         &upsert,
+	}
+
+	product.UpdatedAt = time.Now().UTC()
+
+	var prod models.Product
+	if err := collection.FindOneAndUpdate(ctx, bson.M{"_id": product.ProductID}, bson.M{"$set": product}, &opts).Decode(&prod); err != nil {
+		return nil, errors.Wrap(err, "FindOneAndUpdate failed")
+	}
+
+	log.Printf("UPDATE PRODUCT: %+v", prod)
+
+	return &prod, nil
 }
 
-func (p *productMongoRepo) GetProductByID(ctx context.Context, productID string) (*models.Product, error) {
-	panic("not implemented")
+func (p *productMongoRepo) GetProductByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "productMongoRepo.GetProductByID")
+	defer span.Finish()
+
+	collection := p.mongoDB.Database(productsDB).Collection(productsCollection)
+
+	var prod models.Product
+	if err := collection.FindOne(ctx, bson.M{"_id": productID}).Decode(&prod); err != nil {
+		return nil, errors.Wrap(err, "FindOne failed")
+	}
+
+	log.Printf("GET PRODUCT: %+v", prod)
+
+	return &prod, nil
 }
 
 func (p *productMongoRepo) SearchProducts(ctx context.Context, query string, page, size int64) ([]*models.Product, error) {
