5c319bc2f037e95782b6f010489d37fa9d125a59
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-13 20:55:07 +0200

    Implement UpdateProduct, FindByID, and SearchProduct methods in ProductGRPCService with error handling and OpenTracing support. Update ImageURL handling to use pointers for consistency.

--- product-microservice/internal/product/delivery/gRPC/product_service.go
+++ product-microservice/internal/product/delivery/gRPC/product_service.go
@@ -45,7 +45,7 @@ func (p *ProductGRPCService) CreateProduct(ctx context.Context, req *productServ
 		Name:        req.GetName(),
 		Description: req.GetDescription(),
 		Price:       req.GetPrice(),
-		ImageURL:    req.GetImageUrl(),
+		ImageURL:    &req.ImageUrl,
 		Photos:      req.GetPhotos(),
 		Quantity:    req.GetQuantity(),
 		Rating:      int64(req.GetRating()),
@@ -67,13 +67,73 @@ func (p *ProductGRPCService) CreateProduct(ctx context.Context, req *productServ
 }
 
 func (p *ProductGRPCService) UpdateProduct(ctx context.Context, req *productService.UpdateRequest) (*productService.UpdateResponse, error) {
-	panic("not implemented")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ProductGRPCService.UpdateProduct")
+	defer span.Finish()
+
+	productID, err := primitive.ObjectIDFromHex(req.GetProductId())
+	if err != nil {
+		p.log.Errorf("primitive.ObjectIDFromHex: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	catID, err := primitive.ObjectIDFromHex(req.GetCategoryId())
+	if err != nil {
+		p.log.Errorf("primitve.ObjectIDFromHex: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	product := &models.Product{
+		ProductID:   productID,
+		CategoryID:  catID,
+		Name:        req.GetName(),
+		Description: req.GetDescription(),
+		Price:       req.GetPrice(),
+		ImageURL:    &req.ImageUrl,
+		Photos:      req.GetPhotos(),
+		Quantity:    req.GetQuantity(),
+		Rating:      int64(req.GetRating()),
+	}
+
+	updated, err := p.productUC.UpdateProduct(ctx, product)
+	if err != nil {
+		p.log.Errorf("productUC.UpdateProduct: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	return &productService.UpdateResponse{
+		Product: updated.ToProto(),
+	}, nil
 }
 
 func (p *ProductGRPCService) FindByID(ctx context.Context, req *productService.FindByIDRequest) (*productService.FindByIDResponse, error) {
-	panic("not implemented")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ProductGRPCService.FindByID")
+	defer span.Finish()
+
+	productID, err := primitive.ObjectIDFromHex(req.GetProductId())
+	if err != nil {
+		p.log.Errorf("primitve.ObjectIDFromHex: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	product, err := p.productUC.GetProductByID(ctx, productID)
+	if err != nil {
+		p.log.Errorf("productUC.GetProductByID: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	return &productService.FindByIDResponse{Product: product.ToProto()}, nil
 }
 
 func (p *ProductGRPCService) SearchProduct(ctx context.Context, req *productService.SearchRequest) (*productService.SearchResponse, error) {
-	panic("not implemented")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ProductGRPCService.SearchProduct")
+	defer span.Finish()
+
+	products, err := p.productUC.SearchProducts(ctx, req.GetQuery(), req.GetPage(), req.GetPage())
+	if err != nil {
+		p.log.Errorf("productUC.SearchProducts: %v", err)
+		return nil, grpcerrors.ErrorResponse(err, err.Error())
+	}
+
+	p.log.Info("productUC.SearchProducts: %v", products)
+	return nil, nil
 }
