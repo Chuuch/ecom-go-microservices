f7d01e25500b7b941d0df8e99e6533a6f7779989
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-13 22:17:44 +0200

    Implement SearchProducts method in MongoRepository with pagination support, utilizing the Pagination struct for enhanced query handling and returning a ProductsList for improved data management.

--- product-microservice/internal/product/repository/mongo_repository.go
+++ product-microservice/internal/product/repository/mongo_repository.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/chuuch/product-microservice/internal/models"
 	productErrors "github.com/chuuch/product-microservice/pkg/product_errors"
+	"github.com/chuuch/product-microservice/pkg/utils"
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
@@ -100,6 +101,67 @@ func (p *productMongoRepo) GetProductByID(ctx context.Context, productID primiti
 	return &prod, nil
 }
 
-func (p *productMongoRepo) SearchProducts(ctx context.Context, query string, page, size int64) ([]*models.Product, error) {
-	panic("not implemented")
+func (p *productMongoRepo) SearchProducts(ctx context.Context, query string, pagination *utils.Pagination) (*models.ProductsList, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "productMongoRepo.SearchProducts")
+	defer span.Finish()
+
+	collection := p.mongoDB.Database(productsDB).Collection(productsCollection)
+
+	f := bson.D{
+		{Key: "$or", Value: bson.A{
+			bson.D{{Key: "name", Value: primitive.Regex{Pattern: query, Options: "i"}}},
+			bson.D{{Key: "description", Value: primitive.Regex{Pattern: query, Options: "i"}}},
+		}},
+	}
+
+	count, err := collection.CountDocuments(ctx, f)
+	if err != nil {
+		return nil, errors.Wrap(err, "CountDocuments failed")
+	}
+
+	if count == 0 {
+		return &models.ProductsList{
+			TotalCount: 0,
+			TotalPages: 0,
+			Page:       0,
+			Size:       0,
+			HasMore:    false,
+			Products:   make([]*models.Product, 0),
+		}, nil
+	}
+
+	limit := int64(pagination.GetLimit())
+	skip := int64(pagination.GetOffset())
+	cursor, err := collection.Find(ctx, f, &options.FindOptions{
+		Limit: &limit,
+		Skip:  &skip,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "Find failed")
+	}
+	defer cursor.Close(ctx)
+
+	products := make([]*models.Product, 0, pagination.GetSize())
+	for cursor.Next(ctx) {
+		var prod models.Product
+		if err := cursor.Decode(&prod); err != nil {
+			return nil, errors.Wrap(err, "Decode failed")
+		}
+		products = append(products, &prod)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, errors.Wrap(err, "Find failed")
+	}
+
+	log.Printf("SEARCH PRODUCTS: %+v", products)
+
+	return &models.ProductsList{
+		TotalCount: count,
+		TotalPages: int64(pagination.GetTotalPages(int(count))),
+		Page:       int64(pagination.GetPage()),
+		Size:       int64(pagination.GetSize()),
+		HasMore:    pagination.GetHasMore(int(count)),
+		Products:   products,
+	}, nil
 }
