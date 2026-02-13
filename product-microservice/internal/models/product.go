7bd0a3a7faa9b362e93905d8604dcbf03737216a
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-13 20:54:39 +0200

    Update Product model to use pointer for ImageURL and add GetImageURL helper method. Implement ProductFromProto function for converting gRPC Product to model, including error handling for ObjectID conversion.

--- product-microservice/internal/models/product.go
+++ product-microservice/internal/models/product.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	productService "github.com/chuuch/product-microservice/proto/product"
+	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -15,12 +16,21 @@ type Product struct {
 	Name        string             `json:"name" bson:"name,omitempty" validate:"required,min=3,max=100"`
 	Description string             `json:"description"`
 	Price       float64            `json:"price" bson:"price,omitempty" validate:"required"`
-	ImageURL    string             `json:"image_url" bson:"image_url,omitempty"`
+	ImageURL    *string            `json:"image_url" bson:"image_url,omitempty"`
 	Photos      []string           `json:"photos" bson:"photos,omitempty"`
 	Quantity    int64              `json:"quantity" bson:"quantity,omitempty" validate:"required"`
 	Rating      int64              `json:"rating" bson:"rating,omitempty" validate:"required,min=0,max=10"`
 	CreatedAt   time.Time          `json:"created_at" bson:"created_at,omitempty"`
 	UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at,omitempty"`
+}
+
+// Get Image url helper
+func (p *Product) GetImageURL() string {
+	var imageURL string
+	if p.ImageURL != nil {
+		imageURL = *p.ImageURL
+	}
+	return imageURL
 }
 
 // ToProto Convert product to proto
@@ -31,11 +41,38 @@ func (p *Product) ToProto() *productService.Product {
 		Name:        p.Name,
 		Description: p.Description,
 		Price:       p.Price,
-		ImageUrl:    p.ImageURL,
+		ImageUrl:    p.GetImageURL(),
 		Photos:      p.Photos,
 		Quantity:    p.Quantity,
 		Rating:      int64(p.Rating),
 		CreatedAt:   timestamppb.New(p.CreatedAt),
 		UpdatedAt:   timestamppb.New(p.UpdatedAt),
 	}
+}
+
+// Product from proto to model
+func ProductFromProto(product *productService.Product) (*Product, error) {
+	productID, err := primitive.ObjectIDFromHex(product.GetProductId())
+	if err != nil {
+		return nil, errors.Wrap(err, "primitive.ObjectIDFromHex")
+	}
+
+	catID, err := primitive.ObjectIDFromHex(product.GetCategoryId())
+	if err != nil {
+		return nil, errors.Wrap(err, "primitive.ObjectIDFromHex failed")
+	}
+
+	return &Product{
+		ProductID:   productID,
+		CategoryID:  catID,
+		Name:        product.GetName(),
+		Description: product.GetDescription(),
+		Price:       product.GetPrice(),
+		ImageURL:    &product.ImageUrl,
+		Photos:      product.GetPhotos(),
+		Quantity:    product.GetQuantity(),
+		Rating:      int64(product.GetRating()),
+		CreatedAt:   product.GetCreatedAt().AsTime(),
+		UpdatedAt:   product.GetUpdatedAt().AsTime(),
+	}, nil
 }
