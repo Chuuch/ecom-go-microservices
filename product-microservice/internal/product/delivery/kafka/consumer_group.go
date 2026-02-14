6bd4748557896d07250c67e764f8ce07abfb0e98
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-14 18:16:05 +0200

    Refactor ProductsConsumerGroup to streamline topic handling and worker initialization for create and update product functionalities. This update enhances the clarity of the consumer group implementation by removing the topic parameter and using predefined constants for topic names and worker counts.

--- product-microservice/internal/product/delivery/kafka/consumer_group.go
+++ product-microservice/internal/product/delivery/kafka/consumer_group.go
@@ -20,24 +20,29 @@ const (
 	partitionWatchInterval = 5 * time.Second // how often to watch fo partition changes
 	maxAttempts            = 5               // maximum number of attempts for transient errors
 	dialTimeout            = 3 * time.Minute // timeout for connecting to the broker
+
+	createProductTopic   = "create_product"
+	createProductWorkers = 16
+	updateProductTopic   = "update_product"
+	updateProductWorkers = 16
+
+	productsGropupID = "products_group"
 )
 
 // ProductsConsumerGroup
 type ProductsConsumerGroup struct {
 	Brokers    []string
 	GroupID    string
-	Topic      string
 	log        logger.Logger
-	cfg        config.Config
+	cfg        *config.Config
 	productsUC product.UseCase
 }
 
 // NewProductsConsumerGroup constructor
-func NewProductsConsumerGroup(brokers []string, groupID string, topic string, cfg config.Config, productsUC product.UseCase, log logger.Logger) *ProductsConsumerGroup {
+func NewProductsConsumerGroup(brokers []string, groupID string, cfg *config.Config, productsUC product.UseCase, log logger.Logger) *ProductsConsumerGroup {
 	return &ProductsConsumerGroup{
 		Brokers:    brokers,
 		GroupID:    groupID,
-		Topic:      topic,
 		cfg:        cfg,
 		productsUC: productsUC,
 		log:        log,
@@ -60,21 +65,13 @@ func (c *ProductsConsumerGroup) getNewReader(kafkaURL []string, topic, groupID s
 		Logger:                 kafka.LoggerFunc(c.log.Infof),
 		ErrorLogger:            kafka.LoggerFunc(c.log.Errorf),
 		Dialer: &kafka.Dialer{
-			Timeout:         dialTimeout,
-			LocalAddr:       nil,
-			DualStack:       false,
-			FallbackDelay:   0,
-			KeepAlive:       0,
-			Resolver:        nil,
-			TLS:             nil,
-			SASLMechanism:   nil,
-			TransactionalID: "",
+			Timeout: dialTimeout,
 		},
 	})
 }
 
-func (c *ProductsConsumerGroup) consumeCreateProduct(ctx context.Context, cancel context.CancelFunc, topic string, workerNum int) {
-	r := c.getNewReader(c.Brokers, topic, c.GroupID)
+func (c *ProductsConsumerGroup) consumeCreateProduct(ctx context.Context, cancel context.CancelFunc, groupID string, topic string, workerNum int) {
+	r := c.getNewReader(c.Brokers, topic, groupID)
 	defer cancel()
 	defer func() {
 		if err := r.Close(); err != nil {
@@ -86,15 +83,15 @@ func (c *ProductsConsumerGroup) consumeCreateProduct(ctx context.Context, cancel
 	c.log.Infof("Starting consume create product from topic: %s", topic)
 
 	wg := &sync.WaitGroup{}
-	for range workerNum {
+	for i := range workerNum {
 		wg.Add(1)
-		// TODO: Start a new worker
+		go c.createProductWorker(ctx, cancel, r, wg, i)
 	}
 	wg.Wait()
 }
 
-func (c *ProductsConsumerGroup) consumeUpdateProduct(ctx context.Context, cancel context.CancelFunc, topic string, workerNum int) {
-	r := c.getNewReader(c.Brokers, topic, c.GroupID)
+func (c *ProductsConsumerGroup) consumeUpdateProduct(ctx context.Context, cancel context.CancelFunc, groupID string, topic string, workerNum int) {
+	r := c.getNewReader(c.Brokers, topic, groupID)
 	defer cancel()
 	defer func() {
 		if err := r.Close(); err != nil {
@@ -106,14 +103,14 @@ func (c *ProductsConsumerGroup) consumeUpdateProduct(ctx context.Context, cancel
 	c.log.Infof("Starting consume update product from topic: %s", topic)
 
 	wg := &sync.WaitGroup{}
-	for range workerNum {
+	for i := range workerNum {
 		wg.Add(1)
-		// TODO: Start a new worker
+		go c.updateProductWorker(ctx, cancel, r, wg, i)
 	}
 	wg.Wait()
 }
 
 func (c *ProductsConsumerGroup) RunConsumers(ctx context.Context, cancel context.CancelFunc) {
-	go c.consumeCreateProduct(ctx, cancel, c.Topic, 16)
-	go c.consumeUpdateProduct(ctx, cancel, c.Topic, 16)
+	go c.consumeCreateProduct(ctx, cancel, productsGropupID, createProductTopic, createProductWorkers)
+	go c.consumeUpdateProduct(ctx, cancel, productsGropupID, updateProductTopic, updateProductWorkers)
 }
