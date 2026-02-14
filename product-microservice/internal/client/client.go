f2f3b6699fcafa8f0a1f3117d83af022292bf961
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-14 18:16:24 +0200

    Add client.go to implement Kafka producer for creating products in the product microservice. This new file establishes a connection to Kafka, prepares product messages, and sends them to the 'create_product' topic, enhancing the microservice's messaging capabilities.

---
+++ product-microservice/internal/client/client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/chuuch/product-microservice/internal/models"
+	"github.com/segmentio/kafka-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func main() {
+	conn, err := kafka.DialContext(context.Background(), "tcp", "localhost:9092")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	w := &kafka.Writer{
+		Addr:         kafka.TCP("localhost:9091", "localhost:9092", "localhost:9093"),
+		Topic:        "create_product",
+		Balancer:     &kafka.LeastBytes{},
+		RequiredAcks: -1,
+		MaxAttempts:  3,
+	}
+
+	defer w.Close()
+
+	product := &models.Product{
+		ProductID: primitive.NewObjectID(),
+	}
+
+	productBytes, err := json.Marshal(&product)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg := kafka.Message{
+		Value: productBytes,
+	}
+
+	err = w.WriteMessages(context.Background(), msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
