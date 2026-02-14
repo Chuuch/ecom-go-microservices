bb700beabf614bdafdae2db052b695defd3743d7
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-14 18:41:43 +0200

    Add Kafka writer for dead letter queue in ProductsConsumerGroup. This update introduces a new method to create a Kafka writer with compression and timeout settings, enhancing message handling for product creation and update processes.

--- product-microservice/internal/product/delivery/kafka/consumer_group.go
+++ product-microservice/internal/product/delivery/kafka/consumer_group.go
@@ -9,6 +9,7 @@ import (
 	"github.com/chuuch/product-microservice/internal/product"
 	"github.com/chuuch/product-microservice/pkg/logger"
 	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/compress"
 )
 
 const (
@@ -26,7 +27,11 @@ const (
 	updateProductTopic   = "update_product"
 	updateProductWorkers = 16
 
-	productsGropupID = "products_group"
+	productsGropupID     = "products_group"
+	deadLetterQueueTopic = "dead_letter_queue"
+
+	writerReadTimeout  = 10 * time.Second
+	writerWriteTimeout = 10 * time.Second
 )
 
 // ProductsConsumerGroup
@@ -70,6 +75,24 @@ func (c *ProductsConsumerGroup) getNewReader(kafkaURL []string, topic, groupID s
 	})
 }
 
+// Get new kafka.Writer
+func (c *ProductsConsumerGroup) getNewWriter(topic string) *kafka.Writer {
+	w := &kafka.Writer{
+		Addr:         kafka.TCP(c.Brokers...),
+		Topic:        topic,
+		Balancer:     &kafka.LeastBytes{},
+		RequiredAcks: -1,
+		MaxAttempts:  maxAttempts,
+		ReadTimeout:  writerReadTimeout,
+		WriteTimeout: writerWriteTimeout,
+		Logger:       kafka.LoggerFunc(c.log.Infof),
+		ErrorLogger:  kafka.LoggerFunc(c.log.Errorf),
+		Compression:  compress.Snappy,
+	}
+
+	return w
+}
+
 func (c *ProductsConsumerGroup) consumeCreateProduct(ctx context.Context, cancel context.CancelFunc, groupID string, topic string, workerNum int) {
 	r := c.getNewReader(c.Brokers, topic, groupID)
 	defer cancel()
@@ -80,12 +103,20 @@ func (c *ProductsConsumerGroup) consumeCreateProduct(ctx context.Context, cancel
 		}
 	}()
 
+	w := c.getNewWriter(deadLetterQueueTopic)
+	defer func() {
+		if err := w.Close(); err != nil {
+			c.log.Errorf("w.Close: %v", err)
+			cancel()
+		}
+	}()
+
 	c.log.Infof("Starting consume create product from topic: %s", topic)
 
 	wg := &sync.WaitGroup{}
 	for i := range workerNum {
 		wg.Add(1)
-		go c.createProductWorker(ctx, cancel, r, wg, i)
+		go c.createProductWorker(ctx, cancel, r, w, wg, i)
 	}
 	wg.Wait()
 }
@@ -100,12 +131,20 @@ func (c *ProductsConsumerGroup) consumeUpdateProduct(ctx context.Context, cancel
 		}
 	}()
 
+	w := c.getNewWriter(deadLetterQueueTopic)
+	defer func() {
+		if err := w.Close(); err != nil {
+			c.log.Errorf("w.Close: %v", err)
+			cancel()
+		}
+	}()
+
 	c.log.Infof("Starting consume update product from topic: %s", topic)
 
 	wg := &sync.WaitGroup{}
 	for i := range workerNum {
 		wg.Add(1)
-		go c.updateProductWorker(ctx, cancel, r, wg, i)
+		go c.updateProductWorker(ctx, cancel, r, w, wg, i)
 	}
 	wg.Wait()
 }
