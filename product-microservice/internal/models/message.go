43460e825452635dc827983fd8ba60bdbd86f24b
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-14 18:41:35 +0200

    Update Partition field type in ErrorMessage struct from int32 to int for improved compatibility with messaging systems.

--- product-microservice/internal/models/message.go
+++ product-microservice/internal/models/message.go
@@ -7,7 +7,7 @@ type ErrorMessage struct {
 	MessageID string    `json:"message_id"`
 	Offset    int64     `json:"offset"`
 	Topic     string    `json:"topic"`
-	Partition int32     `json:"partition"`
+	Partition int       `json:"partition"`
 	Error     string    `json:"error"`
 	Time      time.Time `json:"time"`
 }
