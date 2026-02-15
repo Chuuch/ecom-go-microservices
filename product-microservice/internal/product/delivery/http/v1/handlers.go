1e1ba2ce33f1ca823aa50a8bfa5fdf6f2a77589e
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-15 19:59:36 +0200

    Add GetProductByID and SearchProducts handlers to product HTTP handlers
    This commit introduces the GetProductByID and SearchProducts methods in the productHandlers struct, enabling retrieval of a product by its ID and searching for products based on a query. Both methods include error handling and metrics tracking, enhancing the API functionality and observability of the product microservice.

--- product-microservice/internal/product/delivery/http/v1/handlers.go
+++ product-microservice/internal/product/delivery/http/v1/handlers.go
@@ -2,14 +2,17 @@ package v1
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/chuuch/product-microservice/internal/models"
 	"github.com/chuuch/product-microservice/internal/product"
 	httpErrors "github.com/chuuch/product-microservice/pkg/http_errors"
 	"github.com/chuuch/product-microservice/pkg/logger"
+	"github.com/chuuch/product-microservice/pkg/utils"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/opentracing/opentracing-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type productHandlers struct {
@@ -38,17 +41,20 @@ func (h *productHandlers) CreateProduct() echo.HandlerFunc {
 		var prod models.Product
 		if err := c.Bind(&prod); err != nil {
 			h.log.Errorf("c.Bind: %v", err)
+			errorRequests.Inc()
 			return httpErrors.ErrorCtxResponse(c, err)
 		}
 
 		if err := h.validate.StructCtx(ctx, prod); err != nil {
 			h.log.Errorf("validate.StructCtx: %v", err)
+			errorRequests.Inc()
 			return httpErrors.ErrorCtxResponse(c, err)
 		}
 
 		created, err := h.productUC.CreateProduct(ctx, &prod)
 		if err != nil {
 			h.log.Errorf("productUC.CreateProduct: %v", err)
+			errorRequests.Inc()
 			return httpErrors.ErrorCtxResponse(c, err)
 		}
 
@@ -67,16 +73,85 @@ func (h *productHandlers) UpdateProduct() echo.HandlerFunc {
 		var prod models.Product
 		if err := c.Bind(&prod); err != nil {
 			h.log.Errorf("c.Bind: %v", err)
+			errorRequests.Inc()
+			return httpErrors.ErrorCtxResponse(c, err)
+		}
+
+		if err := h.validate.StructCtx(ctx, prod); err != nil {
+			h.log.Errorf("validate.StructCtx: %v", err)
+			errorRequests.Inc()
 			return httpErrors.ErrorCtxResponse(c, err)
 		}
 
 		updated, err := h.productUC.UpdateProduct(ctx, &prod)
 		if err != nil {
 			h.log.Errorf("productUC.UpdateProduct: %v", err)
+			errorRequests.Inc()
 			return httpErrors.ErrorCtxResponse(c, err)
 		}
 
 		successRequests.Inc()
 		return c.JSON(http.StatusOK, updated)
+	}
+}
+
+func (h *productHandlers) GetProductByID() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		span, ctx := opentracing.StartSpanFromContext(c.Request().Context(), "productHandlers.GetProductByID")
+		defer span.Finish()
+
+		getRequests.Inc()
+
+		productID, err := primitive.ObjectIDFromHex(c.QueryParam("product_id"))
+		if err != nil {
+			h.log.Errorf("primitive.ObjectIDFromHex: %v", err)
+			errorRequests.Inc()
+			return httpErrors.ErrorCtxResponse(c, err)
+		}
+
+		product, err := h.productUC.GetProductByID(ctx, productID)
+		if err != nil {
+			h.log.Errorf("productUC.GetProductByID: %v", err)
+			errorRequests.Inc()
+			return httpErrors.ErrorCtxResponse(c, err)
+		}
+
+		successRequests.Inc()
+		return c.JSON(http.StatusOK, product)
+	}
+}
+
+func (h *productHandlers) SearchProducts() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		span, ctx := opentracing.StartSpanFromContext(c.Request().Context(), "productHandlers.SearchProducts")
+		defer span.Finish()
+
+		searchRequests.Inc()
+
+		page, err := strconv.Atoi(c.QueryParam("page"))
+		if err != nil {
+			h.log.Errorf("strconv.Atoi: %v", err)
+			errorRequests.Inc()
+			return httpErrors.ErrorCtxResponse(c, err)
+		}
+
+		size, err := strconv.Atoi(c.QueryParam("size"))
+		if err != nil {
+			h.log.Errorf("strconv.Atoi: %v", err)
+			errorRequests.Inc()
+			return httpErrors.ErrorCtxResponse(c, err)
+		}
+
+		pq := utils.NewPaginationQuery(size, page)
+
+		result, err := h.productUC.SearchProducts(ctx, c.QueryParam("query"), pq)
+		if err != nil {
+			h.log.Errorf("productUC.SearchProducts: %v", err)
+			errorRequests.Inc()
+			return httpErrors.ErrorCtxResponse(c, err)
+		}
+
+		successRequests.Inc()
+		return c.JSON(http.StatusBadRequest, result)
 	}
 }
