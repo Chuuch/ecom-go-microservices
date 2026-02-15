351d741dfbe0c6dc0bec49b4537436f157d09445
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-15 10:13:11 +0200

    Implement gRPC request counter in InterceptorManager to track total incoming requests. This enhancement improves observability by logging the total number of requests processed, contributing to better monitoring of the product microservice's performance.

--- product-microservice/internal/interceptors/manager.go
+++ product-microservice/internal/interceptors/manager.go
@@ -6,8 +6,17 @@ import (
 
 	"github.com/chuuch/product-microservice/config"
 	"github.com/chuuch/product-microservice/pkg/logger"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+)
+
+var (
+	totalRequests = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "products_service_total_requests",
+		Help: "Total number of incoming gRPC requests",
+	})
 )
 
 // InterceptorManager
@@ -30,9 +39,11 @@ func (im *InterceptorManager) Logger(
 	req any,
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (any, error) {
+	totalRequests.Inc()
 	start := time.Now()
 	md, _ := metadata.FromIncomingContext(ctx)
 	reply, err := handler(ctx, req)
+	im.logger.Infof("Total requests: %v", totalRequests)
 	im.logger.Infof("METHOD: %s, REQUEST: %v, RESPONSE: %v, ERROR: %v, TIME: %s, METADATA: %v", info.FullMethod, req, reply, err, time.Since(start), md)
 	return reply, err
 }
