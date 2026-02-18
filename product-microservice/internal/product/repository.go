f61fccaebafc0826b8785ac2136cc56766da5c03
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-18 22:12:13 +0200

    Add RedisRepository interface for product microservice
    This commit introduces a new RedisRepository interface in repository.go, defining methods for setting, retrieving, and deleting products in Redis. This enhancement supports the microservice's caching and data management capabilities, facilitating better integration with Redis.

--- product-microservice/internal/product/repository.go
+++ product-microservice/internal/product/repository.go
@@ -15,3 +15,10 @@ type MongoRepository interface {
 	GetProductByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error)
 	SearchProducts(ctx context.Context, query string, pagination *utils.Pagination) (*models.ProductsList, error)
 }
+
+// RedisRepository Product
+type RedisRepository interface {
+	SetProduct(ctx context.Context, product *models.Product) error
+	GetProductByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error)
+	DeleteProduct(ctx context.Context, productID primitive.ObjectID) error
+}
