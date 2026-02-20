003352b9831c2fa593ca6f63245dc01570347d24
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-02-20 21:22:58 +0200

    Enhance main.go for search microservice with configuration and logging setup
    This commit updates the main.go file to include comprehensive configuration loading and initialization of logging and tracing services. It integrates the config package to load and parse the YAML configuration, initializes the API logger, and sets up Jaeger for distributed tracing using OpenTracing. These enhancements improve observability and error handling in the search microservice.

--- search-microservice/cmd/main.go
+++ search-microservice/cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/chuuch/search-microservice/config"
+	"github.com/chuuch/search-microservice/pkg/logger"
+	"github.com/chuuch/search-microservice/pkg/logger/jaeger"
+	"github.com/opentracing/opentracing-go"
+)
 
 func main() {
 	log.Println("Starting search microservice")
+
+	cfgFile, err := config.LoadConfig("./config/config.yaml")
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("Failed to parse config: %v", err)
+	}
+
+	apiLogger := logger.NewApiLogger(cfg)
+	apiLogger.InitLogger()
+	apiLogger.Info("config", "cfg", cfg)
+	apiLogger.Info("Logger initialized")
+
+	tracer, closer, err := jaeger.InitJaeger(cfg)
+	if err != nil {
+		apiLogger.Fatalf("Failed to initialize Jaeger: %v", err)
+	}
+	defer closer.Close()
+	apiLogger.Info("Jaeger initialized")
+
+	opentracing.SetGlobalTracer(tracer)
+	apiLogger.Info("Opentracing initialized")
 }
