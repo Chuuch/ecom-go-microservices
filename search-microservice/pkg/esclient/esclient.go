8fa190e8700435f3e1358a888d9df018e1a05bd7
chuchulev-sky <daniel@skyeystudio.com>
2026-02-21 18:14:18 +0200

    Add ElasticSearch index and alias management functions to esclient package
    This commit introduces new functions for creating indices and aliases, as well as checking the existence of indices in the esclient package. The ElasticIndex struct is also added to encapsulate index properties, improving the search microservice's capabilities for managing ElasticSearch resources.

--- search-microservice/pkg/esclient/esclient.go
+++ search-microservice/pkg/esclient/esclient.go
@@ -1,12 +1,25 @@
 package esclient
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 )
+
+type ElasticIndex struct {
+	Path  string `mapstructure:"path" validated:"required"`
+	Name  string `mapstructure:"name" validated:"required"`
+	Alias string `mapstructure:"alias" validated:"required"`
+}
+
+func (e *ElasticIndex) String() string {
+	return fmt.Sprintf("Path: %s, Name: %s, Alias: %s", e.Path, e.Name, e.Alias)
+}
 
 func Info(ctx context.Context, esClient *elasticsearch.Client) (*esapi.Response, error) {
 	response, err := esClient.Info(esClient.Info.WithContext(ctx), esClient.Info.WithHuman())
@@ -16,5 +29,67 @@ func Info(ctx context.Context, esClient *elasticsearch.Client) (*esapi.Response,
 	if response.IsError() {
 		return nil, errors.New(response.String())
 	}
+	return response, nil
+}
+
+func CreateIndex(ctx context.Context, esClient *elasticsearch.Client, name string, data []byte) (*esapi.Response, error) {
+	response, err := esClient.Indices.Create(
+		name,
+		esClient.Indices.Create.WithContext(ctx),
+		esClient.Indices.Create.WithBody(bytes.NewReader(data)),
+		esClient.Indices.Create.WithPretty(),
+		esClient.Indices.Create.WithHuman(),
+		esClient.Indices.Create.WithTimeout(3*time.Second),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.IsError() {
+		return nil, errors.New(response.String())
+	}
+
+	return response, nil
+}
+
+func CreateAlias(ctx context.Context, esClient *elasticsearch.Client, indexes []string, name string, data []byte) (*esapi.Response, error) {
+	response, err := esClient.Indices.PutAlias(
+		indexes,
+		name,
+		esClient.Indices.PutAlias.WithContext(ctx),
+		esClient.Indices.PutAlias.WithBody(bytes.NewReader(data)),
+		esClient.Indices.PutAlias.WithPretty(),
+		esClient.Indices.PutAlias.WithHuman(),
+		esClient.Indices.PutAlias.WithTimeout(3*time.Second),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.IsError() {
+		return nil, errors.New(response.String())
+	}
+
+	return response, nil
+}
+
+func Exists(ctx context.Context, esClient *elasticsearch.Client, indexes []string) (*esapi.Response, error) {
+	response, err := esClient.Indices.Exists(
+		indexes,
+		esClient.Indices.Exists.WithContext(ctx),
+		esClient.Indices.Exists.WithPretty(),
+		esClient.Indices.Exists.WithHuman(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.IsError() && response.StatusCode != 404 {
+		return nil, errors.New(response.String())
+	}
+
 	return response, nil
 }
