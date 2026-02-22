7cd5d56f82a06a1acfdd84566c3fc888a80fed15
chuchulev-sky <daniel@skyeystudio.com>
2026-02-22 21:14:16 +0200

    Add search functionality to ElasticRepository
    This commit enhances the ElasticRepository by introducing a new Search method that constructs and executes a multi-match query against an Elasticsearch index. It incorporates a misstype manager to handle common input errors, improving search accuracy. The method also includes logging for the search query and response, facilitating better debugging and monitoring of search operations.

--- search-microservice/internal/product/repository/elastic_repository.go
+++ search-microservice/internal/product/repository/elastic_repository.go
@@ -8,7 +8,10 @@ import (
 
 	"github.com/chuuch/search-microservice/config"
 	"github.com/chuuch/search-microservice/internal/product/domain"
+	"github.com/chuuch/search-microservice/pkg/esclient"
 	"github.com/chuuch/search-microservice/pkg/logger"
+	misstypemanager "github.com/chuuch/search-microservice/pkg/misstype_manager"
+	"github.com/chuuch/search-microservice/pkg/utils"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
@@ -16,9 +19,10 @@ import (
 )
 
 type ElasticRepository struct {
-	log      logger.Logger
-	cfg      *config.Config
-	esClient *elasticsearch.Client
+	log             logger.Logger
+	cfg             *config.Config
+	esClient        *elasticsearch.Client
+	misstypeManager misstypemanager.MisstypeManager
 }
 
 func NewElasticRepository(log logger.Logger, cfg *config.Config, esClient *elasticsearch.Client) *ElasticRepository {
@@ -58,4 +62,79 @@ func (e *ElasticRepository) Index(ctx context.Context, product domain.Product) e
 	}
 	e.log.Info("product indexed successfully %s", response.String())
 	return nil
+}
+
+func (e *ElasticRepository) Search(
+	ctx context.Context,
+	term string,
+	pagination *utils.Pagination,
+) (*domain.ProductSearchResponse, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ElasticRepository.Search")
+	defer span.Finish()
+	span.LogFields(log.String("term", term), log.Object("pagination", pagination))
+
+	shouldQuery := map[string]any{
+		"query": map[string]any{
+			"bool": map[string]any{
+				"should": []map[string]any{
+					{
+						"multi_match": map[string]any{
+							"query":  term,
+							"fields": []string{"title", "description"},
+						},
+					},
+					{
+						"multi_match": map[string]any{
+							"query":  e.misstypeManager.GetMissTypeWord(term),
+							"fields": []string{"title", "description"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	dataBytes, err := json.Marshal(&shouldQuery)
+
+	e.log.Info("search query: %s", shouldQuery)
+	e.log.Info("JSON query: %s", string(dataBytes))
+
+	response, err := e.esClient.Search(
+		e.esClient.Search.WithContext(ctx),
+		e.esClient.Search.WithIndex(e.cfg.ElasticMapping.ProductsIndex.Name),
+		e.esClient.Search.WithBody(bytes.NewReader(dataBytes)),
+		e.esClient.Search.WithPretty(),
+		e.esClient.Search.WithHuman(),
+		e.esClient.Search.WithTimeout(5*time.Second),
+		e.esClient.Search.WithSize(int(pagination.GetSize())),
+		e.esClient.Search.WithFrom(int(pagination.GetOffset())),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.IsError() {
+		return nil, errors.Wrap(errors.New(response.String()), "failed to search")
+	}
+
+	e.log.Info("search response: %s", response.String())
+
+	hits := esclient.ESHits[*domain.Product]{}
+	err = json.NewDecoder(response.Body).Decode(&hits)
+	if err != nil {
+		return nil, err
+	}
+	responseList := make([]*domain.Product, 0, len(hits.Hits.Hits))
+	for i, source := range hits.Hits.Hits {
+		responseList[i] = source.Source
+	}
+
+	e.log.Info("search response list: %v", responseList)
+	return &domain.ProductSearchResponse{
+		List: responseList,
+		PaginationResponse: utils.NewPaginationResponse(hits.Hits.Total.Value, pagination),
+	}, nil
 }
