b8a4e7c543741f88f10844132c4fb63b11566056
chuchulev-sky <daniel@skyeystudio.com>
2026-02-22 21:14:44 +0200

    Add key mappings loading and initialize misstype manager in App
    This commit introduces functionality to load key mappings from a JSON file and initializes the misstype manager within the App struct. The loadKeysMappings method reads the mappings, logs the process, and handles errors appropriately. Additionally, the HTTP server setup is updated to include the misstype manager, enhancing the search microservice's ability to manage keyboard input errors effectively.

--- search-microservice/internal/app/app.go
+++ search-microservice/internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -10,22 +11,63 @@ import (
 	"time"
 
 	"github.com/chuuch/search-microservice/config"
+	"github.com/chuuch/search-microservice/internal/product/repository"
+	v1 "github.com/chuuch/search-microservice/internal/product/transport/http/v1"
+	"github.com/chuuch/search-microservice/internal/product/usecase"
 	"github.com/chuuch/search-microservice/pkg/elastic"
 	"github.com/chuuch/search-microservice/pkg/esclient"
 	"github.com/chuuch/search-microservice/pkg/logger"
 	"github.com/chuuch/search-microservice/pkg/logger/jaeger"
+	"github.com/chuuch/search-microservice/pkg/middlewares"
+	misstypemanager "github.com/chuuch/search-microservice/pkg/misstype_manager"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v5"
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
 )
 
 type App struct {
-	log           logger.Logger
-	cfg           *config.Config
-	doneCh        chan struct{}
-	elasticClient *elasticsearch.Client
-	validate      *validator.Validate
+	log               logger.Logger
+	cfg               *config.Config
+	doneCh            chan struct{}
+	elasticClient     *elasticsearch.Client
+	validate          *validator.Validate
+	echo              *echo.Echo
+	middlewareManager middlewares.MiddlewareManager
+	misstypeManager   misstypemanager.MisstypeManager
+}
+
+func (a *App) loadKeysMappings() (*misstypemanager.KeyboardMisstypeManager, error) {
+	getwd, err := os.Getwd()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get working directory")
+	}
+
+	keysJsonPath := fmt.Sprintf("%s/config/translate.json", getwd)
+	keysJsonPathFile, err := os.Open(keysJsonPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer keysJsonPathFile.Close()
+
+	keysJsonBytes, err := io.ReadAll(keysJsonPathFile)
+	if err != nil {
+		return nil, err
+	}
+
+	a.log.Info("loaded keys mappings file %s", keysJsonPath)
+
+	keyMappings := map[string]string{}
+	if err := json.Unmarshal(keysJsonBytes, &keyMappings); err != nil {
+		return nil, err
+	}
+
+	a.log.Info("loaded %d key mappings", len(keyMappings))
+
+	misstypeManager := misstypemanager.NewMisstypeManager(a.log, keyMappings)
+	return misstypeManager, nil
 }
 
 func NewApp(log logger.Logger, cfg *config.Config) *App {
@@ -34,6 +76,7 @@ func NewApp(log logger.Logger, cfg *config.Config) *App {
 		cfg:      cfg,
 		doneCh:   make(chan struct{}),
 		validate: validator.New(),
+		echo:     echo.New(),
 	}
 }
 
@@ -110,6 +153,13 @@ func (a *App) Run() error {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
+	misstypeManager, err := a.loadKeysMappings()
+	if err != nil {
+		return err
+	}
+	a.misstypeManager = misstypeManager
+	a.log.Info("misstype manager initialized")
+
 	// Initialize Jaeger
 	if a.cfg.Jaeger.Enable {
 		tracer, closer, err := jaeger.InitJaeger(a.cfg)
@@ -120,6 +170,8 @@ func (a *App) Run() error {
 		opentracing.SetGlobalTracer(tracer)
 		a.log.Info("Jaeger initialized")
 	}
+
+	// a.middlewareManager = middlewares.NewMiddlewareManager(a.log, a.cfg, a.getHttpMetricsCb())
 
 	// Initialize Elasticsearch
 	elasticSearchClient, err := elastic.NewElasticSearch(a.cfg)
@@ -140,6 +192,20 @@ func (a *App) Run() error {
 	if err := a.initIndexes(ctx); err != nil {
 		return err
 	}
+
+	elasticRepository := repository.NewElasticRepository(a.log, a.cfg, a.elasticClient)
+	productUsecase := usecase.NewProductUsecase(a.log, a.cfg, elasticRepository)
+	productController := v1.NewProductController(a.log, a.cfg, productUsecase, a.echo.Group(a.cfg.Http.ProductsPath), a.validate)
+	productController.MapRoutes()
+
+	go func() {
+		if err := a.runHttpServer(); err != nil {
+			a.log.Error("Failed to start HTTP server: %v", err)
+			cancel()
+		}
+	}()
+
+	a.log.Info("HTTP server started on port %s", a.cfg.Http.Port)
 
 	<-ctx.Done()
 	a.waitShutDown(3 * time.Second)
