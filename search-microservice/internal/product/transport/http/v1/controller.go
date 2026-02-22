d1bc0e7b3db5e130f382833f504c440af9fca094
chuchulev-sky <daniel@skyeystudio.com>
2026-02-22 17:29:36 +0200

    Add product controller for HTTP handling in v1
    This commit introduces a new productController struct in the controller.go file, implementing the index method for handling product indexing requests via HTTP. It utilizes the echo framework for routing and integrates with the productUsecase for processing. The controller also includes error handling and logging, enhancing the microservice's capability to manage product operations through a RESTful interface.

---
+++ search-microservice/internal/product/transport/http/v1/controller.go
@@ -0,0 +1,60 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/chuuch/search-microservice/config"
+	"github.com/chuuch/search-microservice/internal/product/domain"
+	httpErrors "github.com/chuuch/search-microservice/pkg/http_errors"
+	"github.com/chuuch/search-microservice/pkg/logger"
+	"github.com/go-playground/validator"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v5"
+	"github.com/opentracing/opentracing-go"
+)
+
+type productController struct {
+	log            logger.Logger
+	cfg            *config.Config
+	productUsecase domain.ProductUsecase
+	group          *echo.Group
+	validate       *validator.Validate
+}
+
+func NewProductController(
+	log logger.Logger,
+	cfg *config.Config,
+	productUsecase domain.ProductUsecase,
+	group *echo.Group,
+	validate *validator.Validate) *productController {
+	return &productController{
+		log:            log,
+		cfg:            cfg,
+		productUsecase: productUsecase,
+		group:          group,
+		validate:       validate,
+	}
+}
+
+func (h *productController) index() echo.HandlerFunc {
+	return func(c *echo.Context) error {
+		span, ctx := opentracing.StartSpanFromContext(c.Request().Context(), "productController.index")
+		defer span.Finish()
+
+		var product domain.Product
+		if err := c.Bind(&product); err != nil {
+			h.log.Errorf("failed to bind product: %v", err)
+			return httpErrors.ErrorCtxResponse(c, err, h.cfg.Http.DebugErrorResponse)
+		}
+
+		product.ID = uuid.New().String()
+
+		if err := h.productUsecase.Index(ctx, product); err != nil {
+			h.log.Error("failed to index product: %v", err)
+			return httpErrors.ErrorCtxResponse(c, err, h.cfg.Http.DebugErrorResponse)
+		}
+
+		h.log.Info("product indexed successfully %s", product.ID)
+		return c.JSON(http.StatusCreated, product)
+	}
+}
