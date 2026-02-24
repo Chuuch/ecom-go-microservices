666196fec1ce8fdbd4bbfe9cbc88f5e939f39163
chuchulev-sky <daniel@skyeystudio.com>
2026-02-24 11:41:33 +0200

    Add metrics server functionality to search microservice
    This commit introduces a new metrics server in metrics.go, enabling the search microservice to expose Prometheus metrics. It includes middleware for error handling and tracks HTTP request success and failure metrics, enhancing observability and performance monitoring capabilities.

---
+++ search-microservice/internal/app/metrics.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/chuuch/search-microservice/pkg/middlewares"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func (a *App) runMetrics(cancel context.CancelFunc) {
+	a.metricsServer = echo.New()
+	go func() {
+		a.metricsServer.Use(
+			middleware.RecoverWithConfig(
+				middleware.RecoverConfig{
+					StackSize:         stackSize,
+					DisableStackAll:   false,
+					DisablePrintStack: false,
+				},
+			),
+		)
+		a.metricsServer.GET(a.cfg.Probes.PrometheusPath, echo.WrapHandler(promhttp.Handler()))
+
+		a.log.Info("metrics server started on port %s", a.cfg.Probes.PrometheusPort)
+		if err := a.metricsServer.Start(fmt.Sprintf(":%s", a.cfg.Probes.PrometheusPort)); err != nil {
+			a.log.Error("failed to start metrics server: %v", err)
+			cancel()
+		}
+	}()
+}
+
+func (a *App) getHttpMetricsCb() middlewares.MiddlewareMetricsCb {
+	return func(err error) {
+		if err != nil {
+			a.metrics.ErrorHttpRequestsTotal.Inc()
+		} else {
+			a.metrics.SuccessHttpRequestsTotal.Inc()
+		}
+	}
+}
