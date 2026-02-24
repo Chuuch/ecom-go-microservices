6174e074165aae40ad5f1260fcd6c54cffb6ec83
chuchulev-sky <daniel@skyeystudio.com>
2026-02-24 13:09:38 +0200

    Implement asynchronous indexing for products in search microservice
    This commit adds the IndexAsync method to the product use case, enabling asynchronous indexing of products via an AMQP publisher. The method serializes product data to JSON and publishes it to the configured RabbitMQ exchange, enhancing the microservice's messaging capabilities and improving performance during product indexing operations.

--- search-microservice/internal/product/usecase/usecase.go
+++ search-microservice/internal/product/usecase/usecase.go
@@ -2,26 +2,38 @@ package usecase
 
 import (
 	"context"
+	"encoding/json"
+	"time"
 
 	"github.com/chuuch/search-microservice/config"
 	"github.com/chuuch/search-microservice/internal/product/domain"
 	"github.com/chuuch/search-microservice/pkg/logger"
+	"github.com/chuuch/search-microservice/pkg/rabbitmq"
 	"github.com/chuuch/search-microservice/pkg/utils"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
+	"github.com/pkg/errors"
+	"github.com/rabbitmq/amqp091-go"
 )
 
 type productUsecase struct {
 	log               logger.Logger
 	cfg               *config.Config
 	elasticRepository domain.ElasticRepository
+	amqpPublisher     rabbitmq.AmqpPublisher
 }
 
-func NewProductUsecase(log logger.Logger, cfg *config.Config, elasticRepository domain.ElasticRepository) *productUsecase {
+func NewProductUsecase(
+	log logger.Logger,
+	cfg *config.Config,
+	elasticRepository domain.ElasticRepository,
+	amqpPublisher rabbitmq.AmqpPublisher,
+) *productUsecase {
 	return &productUsecase{
 		log:               log,
 		cfg:               cfg,
 		elasticRepository: elasticRepository,
+		amqpPublisher:     amqpPublisher,
 	}
 }
 
@@ -40,4 +52,25 @@ func (u *productUsecase) Search(ctx context.Context, term string, pagination *ut
 
 	span.LogFields(log.String("term", term), log.Object("pagination", pagination))
 	return u.elasticRepository.Search(ctx, term, pagination)
+}
+
+func (u *productUsecase) IndexAsync(ctx context.Context, product domain.Product) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "productUsecase.IndexAsync")
+	defer span.Finish()
+
+	dataBytes, err := json.Marshal(&product)
+	if err != nil {
+		return errors.Wrap(err, "json.Marshal")
+	}
+
+	return u.amqpPublisher.Publish(
+		ctx,
+		u.cfg.RabbitMQ.ExchangeName,
+		u.cfg.RabbitMQ.BindingKey,
+		amqp091.Publishing{
+			ContentType: "application/json",
+			Body:        dataBytes,
+			Timestamp:   time.Now().UTC(),
+		},
+	)
 }
